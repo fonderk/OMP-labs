4e3b86c118c894ee45c9e732c02d52427a761c88
Ilia Fonderkin <58978371+fonderk@users.noreply.github.com>
2021-04-05 13:53:18 +0300

    Add files via upload

---
+++ Lab.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+#include<omp.h>
+#include<conio.h>
+using namespace std;
+void main ( char **argv)
+{
+#pragma omp parallel num_threads(4)
+	{
+		printf("Hello World!\n");
+	}
+	int n;
+	cin >> n;
+}	
